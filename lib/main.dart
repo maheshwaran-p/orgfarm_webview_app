e1a372d606ba6fccf1f7609937363def37690a51
maheshwaran-p <mpmahesh1022@gmail.com>
2024-10-15 23:54:14 +0530

    force update available

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,119 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:orgfarm/firebase_options.dart';
+import 'package:orgfarm/services/constant.dart';
+import 'package:orgfarm/services/remote_config_service.dart';
+import 'package:orgfarm/utils_service.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
-import 'loading_state.dart'; // Import the LoadingState provider
+import 'loading_state.dart'; 
 
-void main() => runApp(
-      ChangeNotifierProvider(
-        create: (context) => LoadingState(),
-        child: const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: WebViewExample(),
-        ),
-      ),
+void showUpdateAlert(BuildContext context) {
+
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: Text('Update Available'),
+          content: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text('A new version of the app is available. Please update to the latest version.'),
+          ),
+          actions: <Widget>[
+            
+            CupertinoDialogAction(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            CupertinoDialogAction(
+              child: Text('Update'),
+              onPressed: () {
+              if(Platform.isAndroid){
+              Utils.openLink(url: 'https://play.google.com/store/apps/details?id=com.orgfarm.customer');
+                FirebaseAnalytics.instance.logEvent(
+                  name: 'update_popup_shown_in_android',
+                  parameters: {'status': 'success'},
+                );
+
+              }else{
+                Utils.openLink(url: 'https://apps.apple.com/us/app/orgfarm/id1515008376?platform=iphone');
+                FirebaseAnalytics.instance.logEvent(
+                  name: 'update_popup_shown_in_ios',
+                  parameters: {'status': 'success'},
+                );
+              }   
+              },
+            ),
+          ],
+        );
+      },
     );
+  
+}
 
-class WebViewExample extends StatelessWidget {
-  const WebViewExample({super.key});
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+ // (Platform.isAndroid)?
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  //:await Firebase.initializeApp();
+
+  await FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(Constants.isRelease);
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => LoadingState(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: WebViewExample(),
+      ),
+    ),
+  );
+}
+
+class WebViewExample extends StatefulWidget {
+  const WebViewExample({Key? key}) : super(key: key);
+
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+class _WebViewExampleState extends State<WebViewExample> {
+  @override
+  void initState() {
+    super.initState();
+    _checkForUpdates(); // Check for updates when the widget initializes
+  }
+
+  Future<void> _checkForUpdates() async {
+    final remoteConfig = RemoteConfigService();
+    await remoteConfig.setup();
+    final info = await PackageInfo.fromPlatform();
+
+    final localVersion = int.parse(info.version.replaceAll('.', ''));
+  
+    final remoteVersion = Platform.isAndroid? int.parse(remoteConfig.newAndriodAppVersion.replaceAll('.', '')):int.parse(remoteConfig.newIosAppVersion.replaceAll('.', ''));
+    debugPrint('localVersion::::$localVersion');
+    debugPrint('remoteVersion:::::$remoteVersion' );
+
+
+
+    if (localVersion < remoteVersion) {
+      // Show update alert based on the platform
+      showUpdateAlert(context);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,8 +165,7 @@ class __WebViewWidgetState extends State<_WebViewWidget> {
       params = const PlatformWebViewControllerCreationParams();
     }
 
-    final WebViewController controller =
-        WebViewController.fromPlatformCreationParams(params);
+    final WebViewController controller = WebViewController.fromPlatformCreationParams(params);
 
     controller
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_core
+import firebase_remote_config
 import package_info_plus
 import path_provider_foundation
+import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseRemoteConfigPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseRemoteConfigPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
